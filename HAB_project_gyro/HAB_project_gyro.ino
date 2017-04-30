b465a40aaea4b35f8fbda0fabe3b0bb26c22a3ee
Cameron Sullivan <arrowc11@gmail.com>
2017-04-30 11:57:44 -0400

    Final cleaning, added stabilization

--- HAB_project_gyro/HAB_project_gyro.ino
+++ HAB_project_gyro/HAB_project_gyro.ino
@@ -1,5 +1,10 @@
-// Cameron and Dustin's Arduino code for the Sparkfun MPU9250 Inertial Measurement Unit
-// Initial code obtained from:
+/*  Cameron and Dustin's ECE 198 Camera payload stabilization code for the Sparkfun MPU9250 Inertial Measurement Unit
+ *  This code only uses the gyro sensor in the IMU.
+ *  To stabilize the payload, we read the angular velocity in degrees per second, and integrate to obtain heading it degrees.
+ *  With the heading, we use an algorithm to set the servo to the proper angle to rotate the box back to its initial heading.
+ */
+ 
+// Initial IMU code obtained from:
 /* by: Kris Winer
  date: April 1, 2014
  license: Beerware - Use this code however you'd like. If you
@@ -21,8 +26,6 @@
  AD0 ---- GND
  */
 
-//TODO: diagnose commented error, clean up code, add servo code.
-
 #include "quaternionFilters.h"
 #include "MPU9250.h"
 #include <Servo.h>
@@ -34,11 +37,154 @@ Servo servo;
 int intPin = 12;  // These can be changed, 2 and 3 are the Arduinos ext int pins
 int myLed  = 13;  // Set up pin 13 led for toggling
 
+// Global vars used in loop() and stabilize().
+int count = 0;
+int firstTime = 1;
+float desiredHeading;
+bool jumpedRTL = false;
+bool jumpedLTR = false;
+float previousHeading;
+
 MPU9250 myIMU;
+
+
 
 void setup()
 {
-  Wire.begin();
+  servo.attach(3);
+  initIMU();
+}
+
+
+
+void loop()
+{
+  // If intPin goes high, all data registers have new data
+  // On interrupt, check if data ready interrupt
+  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+  {
+
+    myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
+
+    // Calculate the gyro value into actual degrees per second
+    // This depends on scale being set
+    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
+    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
+    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
+  } // if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+
+  // Must be called before updating quaternions.
+  myIMU.updateTime();
+
+  // INTEGRATE TO GET HEADING.
+
+  // STABILIZE
+
+
+  if (!AHRS)
+  {
+    myIMU.delt_t = millis() - myIMU.count;
+    if (myIMU.delt_t > 1000)
+    {
+
+        // Print gyro values in degree/sec
+        Serial.print("GYRO- X: "); Serial.print(myIMU.gx, 3);
+        Serial.print(" ");
+        Serial.print("Y: "); Serial.print(myIMU.gy, 3);
+        Serial.print(" ");
+        Serial.print("Z: "); Serial.print(myIMU.gz, 3);
+        Serial.println("   deg/s");
+
+        myIMU.tempCount = myIMU.readTempData();  // Read the adc values
+        // Temperature in degrees Centigrade
+        myIMU.temperature = ((float) myIMU.tempCount) / 333.87 + 21.0;
+        // Print temperature in degrees Centigrade
+//        Serial.print("Temperature is ");  Serial.print(myIMU.temperature, 1);
+
+      // Reset IMU count to current time.
+      myIMU.count = millis();
+      digitalWrite(myLed, !digitalRead(myLed));  // toggle led
+    }
+  }
+}
+
+
+
+void stabilize(float heading) {
+    int servoReading;
+  
+  float headingDiff = heading - desiredHeading;
+  float change = heading - previousHeading;
+
+  // Cases:
+  // JumpedRTL && desiredHeading < 180
+  // JumpedRTL && desiredHeading > 180
+  // JumpedLTR && desiredHeading < 180
+  // JumpedLTR && desiredHeading > 180
+
+  if (change < -350) {
+    Serial.println("JUMPED LTR");
+    jumpedLTR = true;
+    jumpedRTL = false;
+  }
+  if (change > 350) {
+    Serial.println("JUMPED RTL");
+    jumpedRTL = true;
+    jumpedLTR = false;
+  }
+
+  if (jumpedRTL) {
+    if (desiredHeading < 180) {
+      headingDiff = -((360 - heading) + desiredHeading);
+    }
+    // desiredHeading > 180
+//    else {
+//      headingDiff = desiredHeading - headingDegrees;
+//    }
+  }
+
+  if (jumpedLTR) {
+    if (desiredHeading < 180) {
+      headingDiff = heading - desiredHeading;
+    }
+    // desiredHeading > 180
+    else {
+      headingDiff = 360 - desiredHeading + heading;
+      
+    }
+  }
+
+    if (headingDiff >= -90 && headingDiff <= 90) {
+    // If the heading is to the right (between 1 and 90 degrees), move to the left.
+    if (headingDiff > 0) {
+      servo.write(90 - headingDiff);
+    }
+    // If the heading is to the left (between -90 and 0 degrees), move to the right.
+    else {
+      servo.write(90 + (-headingDiff));
+    }
+    servoReading = servo.read();
+  }
+
+    // Print every second (count is global).
+  if ((count % 100) == 0) {
+    Serial.print("Heading: "); Serial.println(heading);
+    if (jumpedRTL) {
+      Serial.println("jumpedRTL TRUE");
+    }
+    if (jumpedLTR) {
+      Serial.println("jumpedLTR TRUE");
+    }
+//    Serial.print("Heading difference: "); Serial.println(headingDiff);
+//    Serial.print("Servo angle: "); Serial.println(servoReading);
+    Serial.println("");
+  }
+}
+
+
+
+void initIMU(void) {
+    Wire.begin();
   // TWBR = 12;  // 400 kbit/sec I2C speed
   Serial.begin(38400);
   servo.attach(3);
@@ -86,7 +232,7 @@ void setup()
     Serial.print(" I should be 0x");
     Serial.println(0x48, HEX);
 
-// For some reason, this if statement causes the program to stop working. My theory is that this statement is for SPI.
+// For some reason, this if statement causes the program to stop working. My thought is that this statement is for SPI.
 //    if (d != 0x48)
 //    {
 //      // Communication failed, stop here
@@ -96,9 +242,7 @@ void setup()
 //    }
 
     // Get sensor resolutions, only need to do this once
-    myIMU.getAres();
     myIMU.getGres();
-    myIMU.getMres();
 
     delay(2000); // Add delay to see results before serial spew of data
 
@@ -115,68 +259,3 @@ void setup()
   }
 }
 
-void loop()
-{
-  // If intPin goes high, all data registers have new data
-  // On interrupt, check if data ready interrupt
-  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
-  {
-
-    myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
-
-    // Calculate the gyro value into actual degrees per second
-    // This depends on scale being set
-    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
-    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
-    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
-  } // if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
-
-  // Must be called before updating quaternions!
-  myIMU.updateTime();
-
-  if (!AHRS)
-  {
-    myIMU.delt_t = millis() - myIMU.count;
-    if (myIMU.delt_t > 500)
-    {
-
-        // Print gyro values in degree/sec
-        Serial.print("X-gyro rate: "); Serial.print(myIMU.gx, 3);
-        Serial.print(" degrees/sec ");
-        Serial.print("Y-gyro rate: "); Serial.print(myIMU.gy, 3);
-        Serial.print(" degrees/sec ");
-        Serial.print("Z-gyro rate: "); Serial.print(myIMU.gz, 3);
-        Serial.println(" degrees/sec");
-
-        myIMU.tempCount = myIMU.readTempData();  // Read the adc values
-        // Temperature in degrees Centigrade
-        myIMU.temperature = ((float) myIMU.tempCount) / 333.87 + 21.0;
-        // Print temperature in degrees Centigrade
-        Serial.print("Temperature is ");  Serial.print(myIMU.temperature, 1);
-
-      // Reset IMU count to current time.
-      myIMU.count = millis();
-      digitalWrite(myLed, !digitalRead(myLed));  // toggle led
-    } // if (myIMU.delt_t > 500)
-  } 
-  
-  // if (!AHRS)
-  else
-  {
-    // Serial print and/or display at 0.5 s rate independent of data rates
-    myIMU.delt_t = millis() - myIMU.count;
-
-    // update LCD once per second independent of read rate
-    if (myIMU.delt_t > 1000)
-    {
-//        Serial.print("gyro x = ");  Serial.print(myIMU.gx, 2);
-//        Serial.print(" gyro y = "); Serial.print(myIMU.gy, 2);
-        Serial.print(" gyro z = "); Serial.print(myIMU.gz, 2);
-        Serial.println(" deg/s");
-
-      myIMU.count = millis();
-      myIMU.sumCount = 0;
-      myIMU.sum = 0;
-    } // if (myIMU.delt_t > 500)
-  } // if (AHRS)
-}
